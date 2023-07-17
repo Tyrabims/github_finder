a7503f68c8510b1775a3f4ff6fd0a7fca6e4b73c
Tyrabims <abimbola.anibaba@yahoo.com>
2023-07-18 00:25:19 +0100

    More styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,24 @@ import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import { Home } from './components/Home';
-import { About } from './components/About';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  
   return (
-  <>
-  <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
-        </Routes>
-      </Router>
-      
-    </div>
-  </>
-  )
+    <>
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
 }
 export default App;
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,100 +1,93 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { ChangeEvent, FormEvent, useState } from 'react';
 import apiClient from '../services/api-client';
-import styles from "../Forms.module.css"; 
+import styles from '../Forms.module.css';
 
 interface FetchUserProps {
-  "login": string,
-  "id": number,
-  "node_id": string,
-  "avatar_url": string,
-  "gravatar_id": string,
-  "url": string,
-  "html_url": string,
-  "followers_url": string,
-  "following_url": string,
-  "gists_url": string,
-  "starred_url": string,
-  "subscriptions_url": string,
-  "organizations_url": string,
-  "repos_url": string,
-  "events_url": string,
-  "received_events_url": string,
-  "type": string,
-  "site_admin": boolean,
-  "score": number
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  score: number;
 }
 
 const Form = () => {
   const [userdetails, setUserdetails] = useState('');
-  const [users, setUsers] = useState <FetchUserProps[]>([])
-
-
+  const [users, setUsers] = useState<FetchUserProps[]>([]);
 
   //fetch data
   const searchUsername = async () => {
     try {
-        const {data}  = await apiClient.get("/search/users?q="+ userdetails)
-        setUsers(data.items)
-        return data?.item 
-      
-    } catch(error) {
+      const { data } = await apiClient.get('/search/users?q=' + userdetails);
+      setUsers(data.items);
+      return data?.item;
+    } catch (error) {
       return null;
     }
-  }
-  
+  };
+
   //submit
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     if (userdetails) {
       await searchUsername();
     }
-  }
-  const clearBtn = () => {
-    
-  }
-  const moreBtn = <div>
-    
-  </div>
+  };
+  const clearBtn = () => {};
+  const moreBtn = <div></div>;
   return (
-    
     <div className="search">
-      <form
-       className="input-group mb-3">
-        <input 
+      <form className="input-group mb-3">
+        <input
           onChange={(event) => setUserdetails(event.target.value)}
           value={userdetails}
           type="text"
           className="form-control"
           placeholder="Search Users"
         />
-          <button className="btn btn-dark" 
-          type="submit" 
-          onClick={handleSubmit}>
+        <button className="btn btn-dark" type="submit" onClick={handleSubmit}>
           Search
-          </button>
-        </form>
-     
-        <div>
-            {users.map((item) => (
-              <>
-                 <div className="d-grid gap-2">
-                    <button className="btn btn-light" type="button">Clear</button>
-      </div>
-      <div className={styles.container}>
+        </button>
+      </form>
+
+      <div>
+        {users.map((item) => (
+          <>
+            <div className="d-grid gap-2">
+              <button className="btn btn-light" type="button">
+                Clear
+              </button>
+            </div>
+            <div className={styles.container}>
               <img src={item.avatar_url} />
-              <p><b>{item.login}</b></p>
+              <p>
+                <b>{item.login}</b>
+              </p>
               <div>
-              <a className="btn btn-primary" href="#" role="button">More</a>
+                <a className="btn btn-primary" href="#" role="button">
+                  More
+                </a>
               </div>
-        </div>
-              </>
-            ))}
-          
-        </div>
+            </div>
+          </>
+        ))}
       </div>
-    
+    </div>
   );
 };
 
-export default Form
+export default Form;
